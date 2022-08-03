069adeb8d419cd308576ef4a3642dec0445c397b
jon <jonathanyusf@gmail.com>
2022-08-03 14:51:13 -0700

    Use useState to handle folder open/close feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    <Folder name="Desktop">
+      <Folder name="Music">
+        <File name="song.mp3" />
+      </Folder>
+      <File name="dogs.jpeg"/>
+      <File name="cats.png"/> 
+    </Folder>
+    <Folder name="Applications"/>
+  </div>
 }
+
+const Folder = (props) => {
+  const {name, children} = props;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
+
+  return <div onClick={handleClick}>
+    {name}
+    <div style={{ marginLeft: '17px'}}>
+      { isOpen ? children : null }
+    </div>
+  </div>
+}
+
+const File = (props) => {
+  return <div>{props.name}</div>
+}
+
+
 
 export default App;
